242327c5c9167a40469d008d3aeb21df87db5bc8
Samuli Kinnunen <samuli.kinnunen@movya.fi>
2022-01-29 00:16:21 +0200

    Baseline changes

---
+++ Assets/Scripts/GridManagement/Enums/GridCellEnums.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FGJ2022.Grid
+{
+    public enum GridTraversability
+    {
+
+    }
+}
---
+++ Assets/Scripts/GridManagement/GridManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private static GridManager instace;
+    public static GridManager Instance => instace;
+    public Vector2[] GameGrid => gameGrid;
+    private Vector2[] gameGrid;
+
+    private void Start()
+    {
+        if (Instance != null)
+        {
+            instace = this;
+        } else
+        {
+            Debug.LogError("Multiple", this);
+        }
+    }
+
+    public void Initialize()
+    {
+
+    }
+
+}
