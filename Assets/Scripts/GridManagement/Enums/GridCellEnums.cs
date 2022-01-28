2afd5b4f7de86ea0fbeb22313bb87164f3bad966
Samuli Kinnunen <samuli.kinnunen@movya.fi>
2022-01-29 01:11:53 +0200

    Enum feelings

--- Assets/Scripts/GridManagement/Enums/GridCellEnums.cs
+++ Assets/Scripts/GridManagement/Enums/GridCellEnums.cs
@@ -6,6 +6,28 @@ namespace FGJ2022.Grid
 {
     public enum GridTraversability
     {
-
+        debug,
+        easy,
+        medium,
+        hard
+    }
+    public enum GridType
+    {
+        difficult,
+        traversable,
+        easy
+    }
+    public enum GridHostility
+    {
+        hostile,
+        dangerous,
+        safe
+    }
+    public enum GridOwner
+    {
+        player,
+        enemy,
+        neutral,
+        unknown
     }
 }
